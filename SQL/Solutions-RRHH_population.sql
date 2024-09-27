8a2f0c61f4a88ad8b253fd6b44f3fa82516e355b
Gonzalo Mancebo <mancebo.gonzalo27@gmail.com>
2024-09-27 19:35:24 +0100

    Update Solutions-RRHH_population.sql

--- SQL/Solutions-RRHH_population.sql
+++ SQL/Solutions-RRHH_population.sql
@@ -1,1 +1,317 @@
+-- ADD INFORMATION 
 
+ INSERT INTO EMPLOYEE (employee_name, employee_surname, employee_email, employee_phone, employee_dni, date_of_birthday)
+VALUES
+  ('Morgan', 'Bach', 'mbach0@utexas.edu', '(815) 1501482', '698-25-9895', '2017-08-12'),
+  ('Meade', 'Whye', 'mwhye1@devhub.com', '(713) 9332227', '169-70-0293', '1996-10-29'),
+  ('Dorelia', 'Carlett', 'dcarlett2@privacy.gov.au', '(759) 7220633', '687-11-5409', '2011-05-11'),
+  ('Hertha', 'Edgeon', 'hedgeon3@bloglines.com', '(732) 7167895', '775-96-4978', '2019-07-20'),
+  ('Dulcea', 'Ketley', 'dketley4@plala.or.jp', '(950) 2737609', '630-64-3848', '1984-10-19'),
+  ('Markus', 'Castleman', 'mcastleman5@noaa.gov', '(518) 6924857', '883-43-4625', '2017-09-17'),
+  ('Colette', 'Derham', 'cderham6@google.ru', '(872) 9751970', '368-79-6589', '2014-05-09'),
+  ('Vickie', 'Jansema', 'vjansema7@parallels.com', '(941) 8588584', '149-32-3724', '1985-04-08'),
+  ('Benita', 'De Ruggero', 'bderuggero8@yahoo.com', '(506) 5186377', '278-78-2049', '2021-12-29'),
+  ('Flss', 'Corrett', 'fcorrett9@domainmarket.com', '(813) 3329046', '649-45-6426', '2008-02-04'),
+  ('Deny', 'Liley', 'dlileya@ucoz.com', '(194) 6489042', '849-90-5741', '1994-03-28'),
+  ('Lucie', 'Bringloe', 'lbringloeb@surveymonkey.com', '(647) 1413012', '302-23-5561', '1983-10-30'),
+  ('Afton', 'De Meis', 'ademeisc@weebly.com', '(115) 3062669', '631-25-8205', '1991-07-07'),
+  ('Delbert', 'Note', 'dnoted@tripod.com', '(417) 4158931', '242-65-9735', '2005-12-08'),
+  ('Jo', 'Croneen', 'jcroneene@wikispaces.com', '(651) 2981677', '496-32-4078', '2017-08-07'),
+  ('Etty', 'Bowes', 'ebowesf@economist.com', '(944) 7505178', '885-05-0841', '2020-11-09'),
+  ('Cristie', 'Durrant', 'cdurrantg@cloudflare.com', '(591) 4870880', '885-60-3402', '1996-05-30'),
+  ('Sayers', 'Pady', 'spadyh@imdb.com', '(837) 9589379', '506-19-3159', '2022-04-11'),
+  ('Merell', 'Meuse', 'mmeusei@examiner.com', '(242) 4958592', '577-82-7782', '2009-06-16'),
+  ('Ruggiero', 'Blacksell', 'rblacksellj@mapquest.com', '(877) 9651351', '776-93-1015', '2006-07-06'),
+  ('Perice', 'Carnaman', 'pcarnamank@microsoft.com', '(208) 2897637', '217-36-4598', '2003-12-02'),
+  ('Corny', 'Frohock', 'cfrohockl@nydailynews.com', '(561) 2148530', '712-70-9832', '2005-10-05'),
+  ('Zenia', 'Jobson', 'zjobsonm@prweb.com', '(374) 8079286', '829-50-2943', '1993-12-06'),
+  ('Drusi', 'Choppen', 'dchoppenn@ucoz.ru', '(718) 6699471', '593-72-4549', '1980-11-23'),
+  ('Carly', 'Tuohy', 'ctuohyo@bigcartel.com', '(344) 1930370', '702-26-3451', '2011-08-10'),
+  ('Verney', 'Simenon', 'vsimenonp@amazon.com', '(383) 3204667', '169-88-1607', '2006-01-18'),
+  ('Ivory', 'Merrgen', 'imerrgenq@seattletimes.com', '(588) 5306482', '539-02-8336', '1996-08-07'),
+  ('Hort', 'Fetherston', 'hfetherstonr@dailymail.co.uk', '(656) 9500995', '883-74-2032', '2023-07-04'),
+  ('Tony', 'Madgett', 'tmadgetts@miitbeian.gov.cn', '(166) 8340742', '291-37-5546', '1987-04-11'),
+  ('Kristos', 'Ridolfo', 'kridolfot@scientificamerican.com', '(533) 3764294', '689-50-2278', '1997-10-07'),
+  ('Zebedee', 'Beckhouse', 'zbeckhouseu@ycombinator.com', '(497) 1798054', '863-53-8066', '1992-08-07'),
+  ('Poul', 'Cattel', 'pcattelv@msn.com', '(652) 9335853', '278-54-7717', '1981-05-17'),
+  ('Jillie', 'Olphert', 'jolphertw@dmoz.org', '(428) 5844459', '580-15-6866', '2004-09-28'),
+  ('Cullie', 'Gabbidon', 'cgabbidonx@ovh.net', '(643) 2399023', '632-03-2241', '1985-02-09'),
+  ('Sophie', 'Tinker', 'stinkery@telegraph.co.uk', '(182) 2493364', '772-52-0957', '1999-05-01'),
+  ('Amaleta', 'Balls', 'aballsz@smugmug.com', '(558) 2792468', '577-73-1954', '2013-08-03'),
+  ('Creigh', 'Korf', 'ckorf10@marketwatch.com', '(446) 3636346', '630-73-6792', '1989-03-07'),
+  ('Ainsley', 'Walisiak', 'awalisiak11@dedecms.com', '(108) 9402459', '568-14-3306', '1990-05-08'),
+  ('Inger', 'Kirsopp', 'ikirsopp12@state.tx.us', '(604) 9278613', '785-89-9854', '2008-05-23'),
+  ('Olly', 'Peach', 'opeach13@techcrunch.com', '(456) 4144049', '657-32-3287', '2019-02-18'),
+  ('Merrili', 'Bobasch', 'mbobasch14@bbb.org', '(995) 2667887', '851-16-3374', '2005-02-11'),
+  ('Jannelle', 'Van de Castele', 'jvandecastele15@sakura.ne.jp', '(677) 3471339', '708-13-1619', '1982-10-13'),
+  ('Vita', 'Hamelyn', 'vhamelyn16@nhs.uk', '(193) 3728843', '159-71-7828', '1981-01-18'),
+  ('Anton', 'Cissen', 'acissen17@bing.com', '(580) 4635986', '868-37-6261', '2020-03-04'),
+  ('Bette', 'Gilkes', 'bgilkes18@alibaba.com', '(324) 1662475', '661-96-7965', '2016-06-09'),
+  ('Martelle', 'Baude', 'mbaude19@zdnet.com', '(456) 9313796', '414-97-4470', '2000-05-30'),
+  ('Edvard', 'Torre', 'etorre1a@facebook.com', '(257) 7878305', '393-46-5043', '2001-10-23'),
+  ('Klara', 'Stodday', 'kstodday1b@blinklist.com', '(637) 7269828', '275-37-9434', '1996-08-07'),
+  ('Erek', 'Wetherall', 'ewetherall1c@hibu.com', '(931) 1305947', '349-36-2640', '2002-01-25'),
+  ('Cynthy', 'Stirling', 'cstirling1d@sohu.com', '(692) 7885370', '758-94-2837', '2015-09-21');
+
+
+INSERT INTO ASSISTANCE (date_assistance, check_in_time, check_out_time, type_assistance, employee_id) 
+VALUES
+('2024-09-21 08:00:00', '2024-09-21 08:05:00', '2024-09-21 17:00:00', 'FT', 1),
+('2024-09-21 08:00:00', '2024-09-21 08:02:00', '2024-09-21 17:03:00', 'FT', 2),
+('2024-09-21 08:00:00', '2024-09-21 08:01:00', '2024-09-21 16:58:00', 'FT', 3),
+('2024-09-21 08:00:00', '2024-09-21 08:10:00', '2024-09-21 17:05:00', 'FT', 4),
+('2024-09-21 08:00:00', '2024-09-21 08:00:00', '2024-09-21 16:50:00', 'FT', 5),
+('2024-09-21 08:00:00', '2024-09-21 08:03:00', '2024-09-21 17:00:00', 'FT', 6),
+('2024-09-21 08:00:00', '2024-09-21 08:06:00', '2024-09-21 17:02:00', 'FT', 7),
+('2024-09-21 08:00:00', '2024-09-21 08:02:00', '2024-09-21 17:01:00', 'FT', 8),
+('2024-09-21 08:00:00', '2024-09-21 08:01:00', '2024-09-21 17:00:00', 'FT', 9),
+('2024-09-21 08:00:00', '2024-09-21 08:05:00', '2024-09-21 17:03:00', 'FT', 10),
+('2024-09-21 08:00:00', '2024-09-21 08:07:00', '2024-09-21 17:08:00', 'FT', 11),
+('2024-09-21 08:00:00', '2024-09-21 08:04:00', '2024-09-21 17:04:00', 'FT', 12),
+('2024-09-21 08:00:00', '2024-09-21 08:08:00', '2024-09-21 17:09:00', 'FT', 13),
+('2024-09-21 08:00:00', '2024-09-21 08:00:00', '2024-09-21 16:55:00', 'FT', 14),
+('2024-09-21 08:00:00', '2024-09-21 08:02:00', '2024-09-21 17:00:00', 'FT', 15),
+('2024-09-21 08:00:00', '2024-09-21 08:10:00', '2024-09-21 17:10:00', 'FT', 16),
+('2024-09-21 08:00:00', '2024-09-21 08:11:00', '2024-09-21 17:12:00', 'FT', 17),
+('2024-09-21 08:00:00', '2024-09-21 08:04:00', '2024-09-21 17:05:00', 'FT', 18),
+('2024-09-21 08:00:00', '2024-09-21 08:09:00', '2024-09-21 17:07:00', 'FT', 19),
+('2024-09-21 08:00:00', '2024-09-21 08:03:00', '2024-09-21 17:04:00', 'FT', 20),
+('2024-09-21 08:00:00', '2024-09-21 08:00:00', '2024-09-21 17:00:00', 'FT', 21),
+('2024-09-21 08:00:00', '2024-09-21 08:05:00', '2024-09-21 17:01:00', 'FT', 22),
+('2024-09-21 08:00:00', '2024-09-21 08:02:00', '2024-09-21 17:00:00', 'FT', 23),
+('2024-09-21 08:00:00', '2024-09-21 08:00:00', '2024-09-21 16:59:00', 'FT', 24),
+('2024-09-21 08:00:00', '2024-09-21 08:10:00', '2024-09-21 17:03:00', 'FT', 25),
+('2024-09-21 08:00:00', '2024-09-21 08:07:00', '2024-09-21 17:06:00', 'FT', 26),
+('2024-09-21 08:00:00', '2024-09-21 08:03:00', '2024-09-21 17:00:00', 'FT', 27),
+('2024-09-21 08:00:00', '2024-09-21 08:05:00', '2024-09-21 17:02:00', 'FT', 28),
+('2024-09-21 08:00:00', '2024-09-21 08:08:00', '2024-09-21 17:01:00', 'FT', 29),
+('2024-09-21 08:00:00', '2024-09-21 08:01:00', '2024-09-21 17:04:00', 'FT', 30),
+('2024-09-21 08:00:00', '2024-09-21 08:02:00', '2024-09-21 17:08:00', 'FT', 31),
+('2024-09-21 08:00:00', '2024-09-21 08:00:00', '2024-09-21 17:00:00', 'FT', 32),
+('2024-09-21 08:00:00', '2024-09-21 08:03:00', '2024-09-21 16:59:00', 'FT', 33),
+('2024-09-21 08:00:00', '2024-09-21 08:07:00', '2024-09-21 17:00:00', 'FT', 34),
+('2024-09-21 08:00:00', '2024-09-21 08:06:00', '2024-09-21 17:10:00', 'FT', 35),
+('2024-09-21 08:00:00', '2024-09-21 08:01:00', '2024-09-21 17:01:00', 'FT', 36),
+('2024-09-21 08:00:00', '2024-09-21 08:09:00', '2024-09-21 17:03:00', 'FT', 37),
+('2024-09-21 08:00:00', '2024-09-21 08:02:00', '2024-09-21 16:58:00', 'FT', 38),
+('2024-09-21 08:00:00', '2024-09-21 08:04:00', '2024-09-21 17:05:00', 'FT', 39),
+('2024-09-21 08:00:00', '2024-09-21 08:03:00', '2024-09-21 17:04:00', 'FT', 40),
+('2024-09-21 08:00:00', '2024-09-21 08:07:00', '2024-09-21 17:00:00', 'FT', 41),
+('2024-09-21 08:00:00', '2024-09-21 08:00:00', '2024-09-21 17:00:00', 'FT', 42),
+('2024-09-21 08:00:00', '2024-09-21 08:05:00', '2024-09-21 17:05:00', 'FT', 43),
+('2024-09-21 08:00:00', '2024-09-21 08:02:00', '2024-09-21 17:03:00', 'FT', 44),
+('2024-09-21 08:00:00', '2024-09-21 08:06:00', '2024-09-21 16:55:00', 'FT', 45),
+('2024-09-21 08:00:00', '2024-09-21 08:04:00', '2024-09-21 17:02:00', 'FT', 46),
+('2024-09-21 08:00:00', '2024-09-21 08:03:00', '2024-09-21 17:07:00', 'FT', 47),
+('2024-09-21 08:00:00', '2024-09-21 08:01:00', '2024-09-21 17:05:00', 'FT', 48),
+('2024-09-21 08:00:00', '2024-09-21 08:05:00', '2024-09-21 17:01:00', 'FT', 49),
+('2024-09-21 08:00:00', '2024-09-21 08:05:00', '2024-09-21 17:05:00', 'FT', 50);
+
+
+INSERT INTO DEPARTMENT (name_department) 
+VALUES
+('Human Resources'),
+('Finance'),
+('IT'),
+('Marketing'),
+('Sales'),
+('Operations'),
+('Customer Service'),
+('Legal'),
+('Research and Development'),
+('Product Management');
+
+INSERT INTO POSITION (name_position, description_position, id_department) 
+VALUES
+-- Human Resources
+('HR Manager', 'Manages HR', 1),
+('HR Assistant', 'Assists HR tasks', 1),
+('Recruiter', 'Handles recruitment', 1),
+('Data Analyst', 'Analyzes data and provides insights', 1),  
+('Training Coordinator', 'Coordinates training programs', 1),  
+
+
+
+-- Finance
+('Finance Manager', 'Manages finances', 2),
+('Accountant', 'Handles accounts', 2),
+('Financial Analyst', 'Analyzes financials', 2),
+
+-- IT
+('IT Manager', 'Manages IT department', 3),
+('System Administrator', 'Administers systems', 3),
+('Software Developer', 'Develops software', 3),
+('Help Desk Support', 'IT support', 3),
+('Network Engineer', 'Maintains networks', 3),
+
+-- Marketing
+('Marketing Manager', 'Manages marketing', 4),
+('SEO Specialist', 'Handles SEO', 4),
+('Content Creator', 'Creates content', 4),
+('Social Media Manager', 'Manages social media', 4),
+
+-- Sales
+('Sales Manager', 'Manages sales team', 5),
+('Sales Executive', 'Executes sales', 5),
+('Sales Representative', 'Represents sales', 5),
+('Business Development Manager', 'Manages business growth strategies', 5),  
+
+
+-- Operations
+('Operations Manager', 'Manages operations', 6),
+('Logistics Coordinator', 'Coordinates logistics', 6),
+('Procurement Specialist', 'Procures supplies', 6),
+('Quality Assurance Specialist', 'Ensures product quality', 6), 
+
+
+-- Customer Service
+('Customer Service Manager', 'Manages service team', 7),
+('Customer Support Agent', 'Supports customers', 7),
+('Technical Support', 'Provides tech support', 7),
+
+-- Legal
+('Legal Advisor', 'Advises on legal matters', 8),
+('Contract Manager', 'Manages contracts', 8),
+('Paralegal', 'Assists legal team', 8),
+('Compliance Officer', 'Ensures compliance with regulations', 8),
+
+
+-- Research and Development
+('R&D Manager', 'Manages R&D', 9),
+('Research Scientist', 'Conducts research', 9),
+('Product Developer', 'Develops products', 9),
+('Lab Technician', 'Assists in lab', 9),
+
+-- Product Management
+('Product Manager', 'Manages products', 10),
+('Product Analyst', 'Analyzes products', 10),
+('UX/UI Designer', 'Designs user interface', 10),
+('Project Manager', 'Manages projects', 10),
+
+
+('Administrative Assistant', 'Assists administration', 1),
+('Payroll Manager', 'Manages payroll', 2),
+('Database Administrator', 'Administers database', 3),
+('Marketing Coordinator', 'Coordinates marketing', 4),
+('Sales Coordinator', 'Coordinates sales', 5),
+('Operations Coordinator', 'Coordinates operations', 6),
+('Customer Service Coordinator', 'Coordinates service', 7),
+('Legal Assistant', 'Assists legal team', 8),
+('Innovation Manager', 'Manages innovation', 9),
+('Product Coordinator', 'Coordinates products', 10);
+
+INSERT INTO WORKRECORD (start_date, finish_date, employee_id, id_position)
+VALUES
+('2020-01-01', '2022-12-31', 1, 1),
+('2021-02-15', '2024-06-30', 2, 2),
+('2019-03-01', '2024-09-01', 3, 3),
+('2018-04-20', '2023-08-15', 4, 4),
+('2020-05-25', '2024-09-25', 5, 5),
+('2017-06-10', '2023-07-30', 6, 6),
+('2019-07-05', '2024-08-10', 7, 7),
+('2018-08-18', '2023-11-28', 8, 8),
+('2017-09-29', '2022-10-10', 9, 9),
+('2021-10-05', '2024-09-30', 10, 10),
+
+('2020-11-14', '2024-09-14', 11, 11),
+('2019-12-10', '2024-08-10', 12, 12),
+('2018-01-23', '2023-09-15', 13, 13),
+('2020-02-12', '2024-05-20', 14, 14),
+('2020-03-05', '2024-09-25', 15, 15),
+('2019-04-17', '2023-10-17', 16, 16),
+('2021-05-20', '2024-09-30', 17, 17),
+('2018-06-14', '2023-08-19', 18, 18),
+('2019-07-19', '2024-09-29', 19, 19),
+('2018-08-20', '2024-06-30', 20, 20),
+
+('2019-09-25', '2024-09-25', 21, 21),
+('2020-10-14', '2024-09-14', 22, 22),
+('2021-11-05', '2024-09-05', 23, 23),
+('2018-12-30', '2024-07-30', 24, 24),
+('2017-01-10', '2023-05-20', 25, 25),
+('2019-02-22', '2024-03-22', 26, 26),
+('2020-03-15', '2024-06-15', 27, 27),
+('2018-04-08', '2024-09-08', 28, 28),
+('2019-05-19', '2024-08-19', 29, 29),
+('2020-06-27', '2024-09-27', 30, 30),
+
+('2019-07-30', '2024-07-30', 31, 31),
+('2021-08-15', '2024-09-15', 32, 32),
+('2018-09-20', '2024-09-20', 33, 33),
+('2017-10-22', '2023-09-22', 34, 34),
+('2020-11-10', '2024-08-10', 35, 35),
+('2019-12-05', '2024-09-05', 36, 36),
+('2018-01-23', '2023-07-23', 37, 37),
+('2021-02-14', '2024-09-14', 38, 38),
+('2017-03-25', '2023-05-25', 39, 39),
+('2020-04-10', '2024-06-10', 40, 40),
+
+('2018-05-22', '2024-09-22', 41, 41),
+('2019-06-12', '2024-09-12', 42, 42),
+('2020-07-05', '2024-07-05', 43, 43),
+('2021-08-18', '2024-09-18', 44, 44),
+('2019-09-30', '2024-09-30', 45, 45),
+('2017-10-22', '2023-05-22', 46, 46),
+('2020-11-11', '2024-09-11', 47, 47),
+('2021-12-09', '2024-09-09', 48, 48),
+('2018-01-20', '2024-07-20', 49, 49),
+('2018-04-20', '2024-07-21', 50, 50);
+
+
+
+INSERT INTO TRAINING (name_training, description_training, start_date_training, finish_date_training, employee_id)
+VALUES
+('Project Management', 'Training on managing projects effectively', '2021-01-15', '2021-03-15', 1),
+('Advanced Excel', 'Mastering Excel for data analysis', '2020-05-10', '2020-06-10', 2),
+('Communication Skills', 'Enhancing workplace communication skills', '2021-02-20', '2021-04-05', 3),
+('Leadership Training', 'Developing leadership abilities', '2019-09-12', '2019-10-12', 4),
+('Cybersecurity Basics', 'Introduction to cybersecurity practices', '2020-11-05', '2020-12-05', 5),
+('Time Management', 'Improving time management and productivity', '2021-03-01', '2021-04-01', 6),
+('Customer Service', 'Effective customer service training', '2020-08-15', '2020-09-15', 7),
+('Negotiation Skills', 'Learning negotiation strategies', '2019-11-10', '2019-12-10', 8),
+('Public Speaking', 'Training for speaking confidently in public', '2021-06-05', '2021-07-05', 9),
+('Team Building', 'Workshops for improving team cohesion', '2019-04-20', '2019-05-20', 10),
+
+('Technical Writing', 'Writing technical documents clearly', '2020-07-01', '2020-08-01', 11),
+('Data Science', 'Basics of data science and analytics', '2021-02-12', '2021-03-12', 12),
+('Python Programming', 'Introduction to Python programming', '2020-09-15', '2020-11-15', 13),
+('Agile Methodology', 'Learning agile practices for software development', '2021-05-01', '2021-06-01', 14),
+('Financial Analysis', 'Analyzing financial statements', '2020-10-05', '2020-11-05', 15),
+('Digital Marketing', 'Introduction to digital marketing strategies', '2021-07-10', '2021-08-10', 16),
+('Conflict Resolution', 'Resolving conflicts in the workplace', '2019-03-01', '2019-04-01', 17),
+('Cloud Computing', 'Basics of cloud infrastructure', '2020-12-01', '2021-01-01', 18),
+('Supply Chain Management', 'Understanding supply chains', '2021-03-05', '2021-04-05', 19),
+('Database Management', 'Managing and optimizing databases', '2020-02-20', '2020-03-20', 20),
+
+('Emotional Intelligence', 'Understanding and improving emotional intelligence', '2019-05-25', '2019-06-25', 21),
+('Product Management', 'Introduction to managing product lifecycles', '2020-04-10', '2020-05-10', 22),
+('Ethical Hacking', 'Basics of ethical hacking and security', '2021-08-01', '2021-09-01', 23),
+('Content Marketing', 'Learning content creation for marketing', '2019-06-15', '2019-07-15', 24),
+('Java Programming', 'Fundamentals of Java programming', '2020-09-05', '2020-10-05', 25),
+('Mobile App Development', 'Developing mobile applications', '2021-04-10', '2021-05-10', 26),
+('UX Design', 'User experience design fundamentals', '2021-02-18', '2021-03-18', 27),
+('Blockchain Basics', 'Introduction to blockchain technology', '2020-07-20', '2020-08-20', 28),
+('Operations Management', 'Managing operational processes', '2019-10-05', '2019-11-05', 29),
+('Human Resources', 'Training for effective HR management', '2021-01-10', '2021-02-10', 30),
+
+('Sales Strategies', 'Techniques for improving sales', '2020-03-20', '2020-04-20', 31),
+('Machine Learning', 'Basics of machine learning algorithms', '2021-06-15', '2021-07-15', 32),
+('Risk Management', 'Managing risks in business', '2020-05-10', '2020-06-10', 33),
+('Creative Thinking', 'Fostering creativity in the workplace', '2019-12-01', '2019-12-30', 34),
+('Product Design', 'Basics of product design processes', '2021-09-10', '2021-10-10', 35),
+('SQL Databases', 'Introduction to SQL databases', '2020-06-20', '2020-07-20', 36),
+('Change Management', 'Managing organizational changes', '2021-03-12', '2021-04-12', 37),
+('Business Analytics', 'Analyzing data for business decisions', '2020-09-25', '2020-10-25', 38),
+('Digital Transformation', 'Transitioning to digital workflows', '2021-05-05', '2021-06-05', 39),
+('Employee Engagement', 'Strategies for improving employee engagement', '2020-12-15', '2021-01-15', 40),
+
+('Social Media Marketing', 'Learning social media for marketing', '2021-04-20', '2021-05-20', 41),
+('Scrum Master', 'Learning to become a Scrum Master', '2020-08-01', '2020-09-01', 42),
+('Leadership Development', 'Advanced leadership skills', '2021-02-05', '2021-03-05', 43),
+('Presentation Skills', 'Improving presentation techniques', '2020-11-10', '2020-12-10', 44),
+('Graphic Design', 'Introduction to graphic design software', '2021-01-15', '2021-02-15', 45),
+('AI for Business', 'Applications of AI in business processes', '2020-09-10', '2020-10-10', 46),
+('DevOps Fundamentals', 'Learning DevOps principles', '2021-06-05', '2021-07-05', 47),
+('Cyber Threat Management', 'Identifying and managing cyber threats', '2021-03-15', '2021-04-15', 48),
+('Data Visualization', 'Visualizing data for insights', '2020-07-10', '2020-08-10', 49),
+('Communication', 'Communication Skilss', '2020-07-11', '2020-02-10', 50);
